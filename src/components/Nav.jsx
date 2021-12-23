4dd4c20c1981b58bf862026949ffa5fce7b4fa1e
Samet Kızgın <92011854+SametKzgn@users.noreply.github.com>
2021-12-23 11:16:10 +0200

    react task

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -16,7 +16,8 @@ function nav() {
     return (
         <div className="nav-bar">
             <button className="nav-btn">
-                <i  className="fas fa-bars" onClick={sidebarCollapse} > </i> {/* burada ekledıgımız icon a on clıck
+                <i  className="fas fa-bars" onClick={sidebarCollapse} > 
+                </i> {/* burada ekledıgımız icon a on clıck
                                                                          degerı verıyoruz içine ise const da belırledıgımız degeri giriyoruz ki orda ki şeyleri çagırsın */}
                 </button>
             
